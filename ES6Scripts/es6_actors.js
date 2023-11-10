002cb6e27d265f392c0bdf7483614a0e45c511df
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-11-09 19:27:14 -0600

    done with grid game1

--- ES6Scripts/es6_actors.js
+++ ES6Scripts/es6_actors.js
@@ -18,26 +18,27 @@ let academyMembers = [
     {
     memID: 203,
     name: "Bobbie Boots",
-    films: ["Walking Boots", "Hiking Boots",
+    films: ["A Walking Boots", "Hiking Boots",
     "Cowboy Boots"]
     },
-    {
-        memID: 204,
-        name: "Robert Boots",
-       
-        },
+  
    ];
 // Who is the Academy Member whose ID is 187?
 let student = (v => v.memID == 187);
 academyMembers.filter(student).forEach(f => console.log("Student Name: "+f.name));
 
-// Who has been in at least 3 films?
-let student1 = (v => v.films.length <= 3)
-academyMembers.filter(student1).forEach(f => console.log("Member Name: " +f.name))
-// Who has a name that starts with "Bob"?
-let student2 = (v => v.name.includes("Bob"));
-academyMembers.filter(student2).forEach(f => console.log("Member Nmae: "+f.name));
-// HARDER: Which Academy Members have been in a film
-let student3 = (v => v.films.length >0);
-academyMembers.filter(student3).forEach(f => console.log("Member Nmae: "+f.name));
+// // Who has been in at least 3 films?
+let student1 = v => v.films.length >= 3;
+academyMembers.filter(student1).forEach(f => console.log("Member Name: " +f.name));
+
+
+// // Who has a name that starts with "Bob"?
+let student2 = v => v.films.toString().includes("A");
+academyMembers.filter(student2).forEach(f => console.log("Member Nmae: "+f.films));
+// // HARDER: Which Academy Members have been in a film
+ let student3 = v => v.films.length > 0;
+academyMembers.filter(student3).forEach(f => console.log("Member in the film: "+f.name));
 // that starts with "A"
+academyMembers.filter((v => v.films.filter(f => f.indexOf("A")==0).length > 0)).forEach(f => console.log(f.name+" Film Name: "+f.films));
+// let student3 = (v => v.films.length > 0);
+// academyMembers.filter(student3).forEach(f => console.log("Member Name: " + f.name));
